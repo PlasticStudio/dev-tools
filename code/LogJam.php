d5d703b0b9ef357257bd2202263233d386f4f7f5
James Barnsley <james@barnsley.nz>
2016-08-31 14:45:54 +1200

    Creating logjam

--- code/LogJam.php
+++ code/LogJam.php
@@ -9,26 +9,46 @@ class LogJam {
 	static function EnableLog(){
 		static::$enabled = true;	
 	}
+		
+	/**
+	 * Disable logging (enabled by default)
+	 **/
+	static function DisableLog(){
+		static::$enabled = false;	
+	}
 	
 	/**
 	 * Log something into our log file
 	 * @param $environment = string (minimum environment state for logging this message)
 	 * @param $message = string (the message body)
 	 **/
-	static function Log( $message = '', $environment = 'Dev' ){	
+	static function Log( $message = '', $environment = 'dev' ){
+
+		// only proceed if LogJam is enabled
+		if( static::$enabled ){
+			
+			// make sure our log level matches our environment level, otherwise break out
+			if( Director::isLive() && $environment != 'live' ){
+				return false;
+			}else if( Director::isTest() && ( $environment != 'live' || $environment != 'test' ) ){
+				return false;
+			}
+			
+			// trace the origin call
+			$trace = SS_Backtrace::filtered_backtrace();
+			$trace = $trace[0];		
+			$file = isset($trace['file']) ? $trace['file'] : '';
+			$line = isset($trace['line']) ? $trace['line'] : '';
 		
-		// trace the origin call
-		$trace = SS_Backtrace::filtered_backtrace();
-		$trace = $trace[0];		
-		$file = isset($trace['file']) ? $trace['file'] : '';
-		$line = isset($trace['line']) ? $trace['line'] : '';
-	
-		// construct the line in our file
-		$output = 'LogJam: ';
-		$output.= $message;
-		$output.= ' (line '.$line.' in '.$file.')';
+			// construct the line in our file
+			$output = 'LogJam: ';
+			$output.= $message;
+			$output.= ' (line '.$line.' in '.$file.')';
+			
+			// piggy-back PHP error_log
+			return error_log( $output );
+		}
 		
-		// piggy-back PHP error_log
-		error_log( $output );
+		return false;
 	}
 }
